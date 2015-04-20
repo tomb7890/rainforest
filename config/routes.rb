20cacce21fc4265f63a5202f7b4777e58e58b0c4
Tom Billingsley <tom.billingsley@gmail.com>
2015-04-20 16:16:58 -0400

    Generating the products controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
+   get 'products' => 'products#index', as: 'products'
+   post 'products' => 'products#create'
+   get 'products/new' => 'products#new', as: 'new_product'
+   get 'products/:id/edit' => "products#edit", as: "edit_product"
+   get 'products/:id' => 'products#show', as: 'product'
+   patch 'products/:id' => "products#update"
+   put 'products/:id' => "products#update"
+   delete 'products/:id' => 'products#destroy'
+
+  root 'products#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
