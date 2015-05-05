ab85f44d6b941202ef9eb4cd2f888383adea410f
Tom Billingsley <tom.billingsley@gmail.com>
2015-05-05 17:55:47 -0400

    removed redundancy

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,17 +1,11 @@
 $(document).on('ready page:load', function() {
-
-$(document).on('ready page:load', function() {
   $('#search-form').submit(function(event) {
     event.preventDefault();
     var searchValue = $('#search').val();
-
       $.get('/products?search=' + searchValue)
       .done(function(data){
         console.log(data);
         $('#products').html(data);
       });
   });
-});
-
-
 });
