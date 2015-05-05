9f572a1a8bcea0cf7f54dbe9c3dc995828f25388
Tom Billingsley <tom.billingsley@gmail.com>
2015-05-05 16:49:28 -0400

    new improved AJAX functionality

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,23 +1,19 @@
 $(document).on('ready page:load', function() {
 
-  function display_search_results() {
-    // readyState === 4 means that the asynchronous request completed successfully
-    if (this.readyState === 4) {
-      console.log(this);
-      document.getElementById('products').innerHTML = this.responseText;
-    }
-  }
-
-  var form = document.getElementById('search-form');
-  form.addEventListener('submit', function(event) {
+$(document).on('ready page:load', function() {
+  $('#search-form').submit(function(event) {
     event.preventDefault();
-    var searchValue = document.getElementById('search').value;
+    var searchValue = $('#search').val();
 
-    var xhr = new XMLHttpRequest();
-    xhr.onload = display_search_results;
-    xhr.open('GET', '/products?search=' + searchValue, true);
-    xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-    xhr.send();
+    $.ajax({
+      url: '/products?search=' + searchValue,
+      type: 'GET',
+      dataType: 'html'
+    }).done(function(data){
+      $('#products').html(data);
+    });
   });
+});
+
 
 });
