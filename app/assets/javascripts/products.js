5345974cef7026b69697e08203b54b1db3709edc
Tom Billingsley <tom.billingsley@gmail.com>
2015-05-06 16:29:05 -0400

    getScript

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -2,10 +2,9 @@ $(document).on('ready page:load', function() {
   $('#search-form').submit(function(event) {
     event.preventDefault();
     var searchValue = $('#search').val();
-      $.get('/products?search=' + searchValue)
-      .done(function(data){
-        console.log(data);
-        $('#products').html(data);
-      });
+
+      // inside the submit event callback, replace the $.get portion with the following
+    $.getScript('/products?search=' + searchValue);
+
   });
 });
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,13 +6,13 @@ class ProductsController < ApplicationController
       Product.all
     end
 
-    if request.xhr?
-      render @products
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 
   def show
-    # @product = Product.find(params[:id])
     @product = Product.find(params[:id])
 
     if current_user
