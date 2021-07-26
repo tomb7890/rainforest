7450fdbbbe174d89c19206adf76f04125c51e40f
Tom Billingsley <tom.billingsley@gmail.com>
2021-07-26 11:14:04 -0400

    Get ReviewsControllerTest working

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,32 +7,15 @@ before_action :ensure_logged_in, only: [:create, :destroy]
   end
 
   def create
-
     @review = @product.reviews.build(review_params)
     @review.user = current_user
-
-    # Check out this article on [.build](http://stackoverflow.com/questions/783584/ruby-on-rails-how-do-i-use-the-active-record-build-method-in-a-belongs-to-rel)
-    # You could use a longer alternate syntax if it makes more sense to you
-    #
-    # @review = Review.new(
-    #   comment: params[:review][:comment],
-    #   product_id: @product.id,
-    #   user_id: current_user.id
-    # )
-
-    # if @review.save
-    #   redirect_to products_path, notice: 'Review created successfully'
-    # else
-    #   render 'products/show'
-    # end
-
     respond_to do |format|
       if @review.save
         format.html { redirect_to product_path(@product.id), notice: 'Review added.' }
-        format.js {} # This will look for app/views/reviews/create.js.erb
+        format.js {}
       else
         format.html { render 'products/show', alert: 'There was an error.'  }
-        format.js {} # This will look for app/views/reviews/create.js.erb
+        format.js {}
       end
     end
   end
--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,19 +1,36 @@
 require 'test_helper'
 
 class ReviewsControllerTest < ActionController::TestCase
-  test "should get show" do
-    get :show
+  test "the show action" do
+    product = products(:one)
+    review = reviews(:two)
+    get :show, params: { product_id: product.id, id: review.id    }
     assert_response :success
   end
 
-  test "should get create" do
-    get :create
-    assert_response :success
+  test "review creation" do
+    user  = users(:john)
+    product = products(:one)
+    review = reviews(:two)
+    review.user = user
+    review.product = product
+    product_id = product.id
+    post :create, params: { product_id: product.id,
+                            review:
+                              {
+                                comment: "This product is awesome",
+                                product_id: product.id,
+                                user_id: user.id
+                              }
+                          }
+
+    assert_redirected_to new_session_path
   end
 
-  test "should get destroy" do
-    get :destroy
-    assert_response :success
+  test "review destruction" do
+    product = products(:one)
+    review = reviews(:two)
+    get :destroy, params: { product_id: product.id, id: review.id   }
+    assert_redirected_to new_session_path
   end
-
 end
