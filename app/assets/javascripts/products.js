da23fd3daab404e15b8704826d35d5399f7479d4
Tom Billingsley <tom.billingsley@gmail.com>
2021-07-28 16:39:28 -0400

    Disable infinite scrolling for now

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -8,11 +8,11 @@ $(document).on('ready page:load', function() {
   });
 });
 
-$(window).scroll(function() {
-    var url = $('.pagination span.next').children().attr('href');
-    if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50)
-    {
-        $('.pagination').text("Fetching more products...");
-        return $.getScript(url);
-    }
-});
+// $(window).scroll(function() {
+//     var url = $('.pagination span.next').children().attr('href');
+//     if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50)
+//     {
+//         $('.pagination').text("Fetching more products...");
+//         return $.getScript(url);
+//     }
+// });
