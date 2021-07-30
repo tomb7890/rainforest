b9ecddd5677b34524533933c91f3532101cd0e6d
Tom Billingsley <tom.billingsley@gmail.com>
2021-07-30 11:32:13 -0400

    Use Rails 5 security options

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,10 @@ module Rainforest
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.action_controller.per_form_csrf_tokens = true
+    config.action_controller.forgery_protection_origin_check = true
+    config.ssl_options = { hsts: { subdomains: true } }
+    
   end
 end
