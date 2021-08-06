2f7b929709ea2f8f37361fcba83df6b15ec2b81f
Tom Billingsley <tom.billingsley@gmail.com>
2021-08-06 10:03:56 -0400

    Move javascript out of html

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,12 +1,28 @@
 $(document).on('ready page:load', function() {
+
+    
   $('#search-form').submit(function(event) {
     event.preventDefault();
     var searchValue = $('#search').val();
-
     $.getScript('/products?search=' + searchValue);
-
   });
+
+
+    $("#new_review")
+        .on('ajax:beforeSend', function() { // set this code to happen just before the ajax request is made
+            $("input[type='submit']") // make changes to the submit button
+		.val('Saving...') // change the text on the button
+		.attr('disabled', 'disabled'); // disable the button
+        })
+        .on('ajax:complete', function() {
+            $("input[type='submit']")
+		.val('Create Review')
+		.removeAttr('disabled');
+        });
+
+    
 });
+
 
 // $(window).scroll(function() {
 //     var url = $('.pagination span.next').children().attr('href');
