adecad61ca69d38ba20ba6a46f98db2fd4a14550
Tom Billingsley <tom.billingsley@gmail.com>
2021-08-10 12:54:58 -0400

    Use explicit table reference

--- db/migrate/20210805130005_add_owner_ref_to_products.rb
+++ db/migrate/20210805130005_add_owner_ref_to_products.rb
@@ -1,5 +1,5 @@
 class AddOwnerRefToProducts < ActiveRecord::Migration[5.2]
   def change
-    add_reference :products, :owner, foreign_key: true
+    add_reference( :products, :owner, foreign_key:  { to_table: :users }) 
   end
 end
