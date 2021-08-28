63039b36628f8602334d3086f347429b7f5c0522
Tom Billingsley <tom.billingsley@gmail.com>
2021-08-28 09:14:52 -0400

    Rails5 disables form buttons on submission

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -4,23 +4,21 @@ $(document).on('ready page:load', function() {
   $('#search-form').submit(function(event) {
     event.preventDefault();
     var searchValue = $('#search').val();
-    $.getScript('/products?search=' + searchValue);
+    $.getScript('/products?search=' + searchValue)
+
+      .done(function( script, textStatus ) {
+	$button = $('#products-search-button')
+	$.rails.enableElement($button)
+	$button.removeAttr('disabled')
+      })
+    
+      .fail(function( jqxhr, settings, exception ) {
+	console.log("product search triggered ajaxError handler.");
+      });
+
   });
 
-
-    $("#new_review")
-        .on('ajax:beforeSend', function() { // set this code to happen just before the ajax request is made
-            $("input[type='submit']") // make changes to the submit button
-		.val('Saving...') // change the text on the button
-		.attr('disabled', 'disabled'); // disable the button
-        })
-        .on('ajax:complete', function() {
-            $("input[type='submit']")
-		.val('Create Review')
-		.removeAttr('disabled');
-        });
-
-    
+  
 });
 
 
