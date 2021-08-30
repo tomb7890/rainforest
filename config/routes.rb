4687a33a1428b3a46aae2bb59817e3fd374c0600
Tom Billingsley <tom.billingsley@gmail.com>
2021-08-30 17:41:06 -0400

    Add signup and login/out routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
 
+  get 'logout' => 'sessions#destroy'
+  get 'login' => 'sessions#new'
+  get 'signup' => 'users#new'
+
   root 'products#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
