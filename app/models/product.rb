b4580dd08969443c6dc4e8a3dbe55639fe6a9513
Tom Billingsley <tom.billingsley@gmail.com>
2021-08-30 11:29:25 -0400

    Reject negative prices

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,6 +7,7 @@ class Product < ApplicationRecord
 
   validates :description, :name, presence: true
   validates :price_in_cents, numericality: {only_integer: true}
+  validates :price_in_cents, :numericality => { :greater_than_or_equal_to => 1 } 
 
   def formatted_price
     price_in_dollars = price_in_cents.to_f / 100
