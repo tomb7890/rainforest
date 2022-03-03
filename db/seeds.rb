e9b4ab4713675104ec7a24a9e43d979492e7189c
Tom Billingsley <tom.billingsley@gmail.com>
2022-03-03 11:25:28 -0500

    Improve db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,54 @@
 require 'csv'
-data=CSV.read("data/jcpenney_com-ecommerce_sample.csv", headers: true)
+require 'faker'
 
-def create_a_user
+for b in 0..20 do
+  fn = Faker::Name.name
+  fe = Faker::Internet.email(name: fn)
   User.create!({
-                 email: "jcpenny.shopper@example.com",
-                 name: "Susie McDonald",
+                 email: fe,
+                 name: fn,
                  password: "pass",
                  password_confirmation: "pass"
-               }
-              )
+               })
 end
 
-index = 0
-while true
-  index = index + 1
+data=CSV.read("data/jcpenney_com-ecommerce_sample.csv", headers: true)
+table = data.select{|row|
 
-  unless data[index]['name_title'].blank? ||
-         data[index]['description'].blank? ||
-         data[index]['list_price'].blank?
+  !row['list_price'].blank? &&
+  !row['description'].blank? &&
+  !row['name_title'].blank? &&
+  !row['total_number_reviews'].blank? &&
+  row['total_number_reviews'].to_i > 2 }.sample(500)
 
-    unless Product.find_by(description: data[index]['description'])
-      Product.create({
-                       name: data[index]['name_title'],
-                       description: data[index]['description'],
-                       price_in_cents: data[index]['list_price'].to_i * 100
+
+
+def random_user
+  offset = rand(User.count)
+  User.offset(offset).first
+end
+
+table.each do|row|
+
+  unless Product.find_by(description: row['description'])
+    prod = Product.new({
+                         name: row['name_title'],
+                         description: row['description'],
+                         price_in_cents: row['list_price'].to_i * 100
                      }
                     )
-    end
 
-    if Product.all.size > 300
-      break
+    prod.owner = random_user
+    prod.save
+
+    reviewstring = row["Reviews"]
+
+    reviews_array = JSON.parse(reviewstring.gsub('=>', ':'))['review']
+
+    for r in reviews_array
+      review = Review.new(product: prod, user: random_user)
+      review.comment = r.values.last
+      review.save
     end
   end
 end
