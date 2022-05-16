203d7a1ad170dabebcdc612aa3a1e93f178541a0
Tom Billingsley <tom.billingsley@gmail.com>
2022-05-16 11:14:30 -0400

    Add author and date methods to present user-friendly data

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,19 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :product
   validates :comment, :presence => true
+
+  def author
+    unless user.blank? 
+      unless user.name.blank? 
+        return user.name
+      end
+    else
+      "Anonymous"
+    end
+  end
+
+  def date
+    created_at.strftime("%B %d, %Y")
+  end
+  
 end
